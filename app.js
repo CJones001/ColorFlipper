c8f3109ef5a4c3822d38144f2d0f0d4a860ea521
CJones001 <147557670+CJones001@users.noreply.github.com>
2025-01-08 20:26:38 -0700

    Added js for selecting id's

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// Get all elements with the id btn
+const btn = document.getElementById('btn');
+
+// Get the first element with the id color
+const color = document.querySelector(".color");
+
+// Generate a random number when number is clicked
+btn.addEventListener("click", function () {
+    // Get random number between 0 - 3
+    const randomNumber = 2;
+    document.body.style.backgroundColor
+})
